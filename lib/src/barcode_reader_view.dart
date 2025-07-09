7a1dd786f5030c4212a4faaa2f95de1507833064
frng\a.rezaee <arezaee79@gmail.com>
2025-07-09 18:25:09 +0330

    used PlatformViewLink instead of AndroidView

--- lib/src/barcode_reader_view.dart
+++ lib/src/barcode_reader_view.dart
@@ -1,16 +1,16 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
 import 'package:flutter/services.dart';
 import 'barcode_reader_controller.dart';
 import 'barcode_reader_platform_interface.dart';
 
 class BarcodeReaderView extends StatefulWidget {
-  const BarcodeReaderView({
-    super.key,
-    required this.onScannedBarcode,
-    required this.controller,
-  });
+  const BarcodeReaderView({super.key, required this.onScannedBarcode, required this.controller});
 
   final Null Function(String barcode) onScannedBarcode;
   final BarcodeReaderController controller;
@@ -25,20 +25,37 @@ class _BarcodeReaderViewState extends State<BarcodeReaderView> {
   @override
   void initState() {
     super.initState();
-    BarcodeReaderPlatform.instance.initBarcodeCallback(onScannedBarcode: (barcode) {
-      widget.onScannedBarcode(barcode);
-    });
+    BarcodeReaderPlatform.instance.initBarcodeCallback(
+      onScannedBarcode: (barcode) {
+        widget.onScannedBarcode(barcode);
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final platformView =
         Platform.isAndroid
-            ? AndroidView(
+            ? PlatformViewLink(
               viewType: viewType,
-              layoutDirection: TextDirection.ltr,
-              creationParams: {},
-              creationParamsCodec: const StandardMessageCodec(),
+              surfaceFactory: (context, controller) {
+                return AndroidViewSurface(
+                  controller: controller as AndroidViewController,
+                  gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+                  hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+                );
+              },
+              onCreatePlatformView: (params) {
+                return PlatformViewsService.initExpensiveAndroidView(
+                    id: params.id,
+                    viewType: viewType,
+                    layoutDirection: TextDirection.ltr,
+                    creationParams: {},
+                    creationParamsCodec: const StandardMessageCodec(),
+                  )
+                  ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
+                  ..create();
+              },
             )
             : UiKitView(
               viewType: viewType,
