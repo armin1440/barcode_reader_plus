b9d63321f487027d030b3217a7c9a85cf0cd53d7
frng\a.rezaee <arezaee79@gmail.com>
2025-07-09 12:01:56 +0330

    minor changes

--- test/barcode_reader_method_channel_test.dart
+++ test/barcode_reader_method_channel_test.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:barcode_reader/barcode_reader_method_channel.dart';
-
-void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  MethodChannelBarcodeReader platform = MethodChannelBarcodeReader();
-  const MethodChannel channel = MethodChannel('barcode_reader');
-
-  setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
-  });
-
-  tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
-  });
-
-}
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:barcode_reader/barcode_reader_method_channel.dart';
+//
+// void main() {
+//   TestWidgetsFlutterBinding.ensureInitialized();
+//
+//   MethodChannelBarcodeReader platform = MethodChannelBarcodeReader();
+//   const MethodChannel channel = MethodChannel('barcode_reader');
+//
+//   setUp(() {
+//     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+//       channel,
+//       (MethodCall methodCall) async {
+//         return '42';
+//       },
+//     );
+//   });
+//
+//   tearDown(() {
+//     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+//   });
+//
+// }
